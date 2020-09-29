ce46753bc7d37d57fadb5cee0534466c4c742d5f
Brent Mitton <brent.mitton+github@gmail.com>
2020-09-29 13:36:40 -0400

    cleaner tests

--- pkg/store/postgres/postgresStore_test.go
+++ pkg/store/postgres/postgresStore_test.go
@@ -13,6 +13,8 @@ import (
 
 var speedleStore pms.PolicyStoreManager
 var storeConfig *cfg.StoreConfig
+var mockPolicy pms.Policy
+var mockRolePolicy pms.RolePolicy
 
 func TestMain(m *testing.M) {
 	os.Exit(testMain(m))
@@ -24,11 +26,31 @@ func testMain(m *testing.M) int {
 	if err != nil {
 		log.Fatal("fail to read config file", err)
 	}
+
+	// init the speedle store to use across tests
 	speedleStore, err = store.NewStore(storeConfig.StoreType, storeConfig.StoreProps)
 	if err != nil {
 		fmt.Println("Could not initialize postgres store ")
 		return 1
 	}
+
+	// init a mock policy to use in multiple tests
+	mockPolicy.Name = "policy1"
+	mockPolicy.Effect = "grant"
+	mockPolicy.Permissions = []*pms.Permission{
+		{
+			Resource: "/node1",
+			Actions:  []string{"get", "create", "delete"},
+		},
+	}
+	mockPolicy.Principals = [][]string{{"user:Alice"}}
+
+	// init a mock role policy to use in multiple tests
+	mockRolePolicy.Name = "rp1"
+	mockRolePolicy.Effect = "grant"
+	mockRolePolicy.Roles = []string{"role1"}
+	mockRolePolicy.Principals = []string{"user:Alice"}
+
 	return m.Run()
 }
 
@@ -65,43 +87,75 @@ func TestWriteReadPolicyStore(t *testing.T) {
 
 }
 
-func TestManagePolicies(t *testing.T) {
-
-	speedleStore.DeleteService("service1")
-
-	app := pms.Service{Name: "service1", Type: pms.TypeApplication}
+func createTestService(t *testing.T, serviceName string) {
+	app := pms.Service{Name: serviceName, Type: pms.TypeApplication}
 	err := speedleStore.CreateService(&app)
 
 	if err != nil {
-		t.Fatal("failed to create application", err)
+		t.Fatal("failed to create application: ", err)
 	}
+}
+func TestCreateGetService(t *testing.T) {
 
-	var policy pms.Policy
-	policy.Name = "policy1"
-	policy.Effect = "grant"
-	policy.Permissions = []*pms.Permission{
-		{
-			Resource: "/node1",
-			Actions:  []string{"get", "create", "delete"},
-		},
-	}
+	// make sure that the service doesn't already exist
+	speedleStore.DeleteService("service1")
 
-	policy.Principals = [][]string{{"user:Alice"}}
-	policyR, err := speedleStore.CreatePolicy("service1", &policy)
+	// create the service
+	createTestService(t, "service1")
+
+	// check to make sure that we can get the service back from the store
+	appResult, err := speedleStore.GetService("service1")
+
 	if err != nil {
-		t.Fatal("failed to create policy: ", err)
+		t.Fatal("could not get service back from store", err)
 	}
 
-	policyR1, err := speedleStore.GetPolicy("service1", policyR.ID)
-	t.Log(policyR1)
+	if pms.TypeApplication != appResult.Type {
+		t.Fatal("did not get correct service back from store", err)
+	}
+}
+
+func TestCreateGetPolicy(t *testing.T) {
+	speedleStore.DeleteService("service1")
+	createTestService(t, "service1")
+	createPolicyResult, err := speedleStore.CreatePolicy("service1", &mockPolicy)
+	if err != nil {
+		t.Fatal("failed to create policy", err)
+	}
+
+	getPolicyResult, err := speedleStore.GetPolicy("service1", createPolicyResult.ID)
 	if err != nil {
 		t.Fatal("failed to get policy: ", err)
 	}
+	if getPolicyResult.Name != "policy1" {
+		t.Fatal("did not get right policy from GetPolicy")
+	}
+}
+
+func TestListAllPolicies(t *testing.T) {
+	speedleStore.DeleteService("service1")
+	createTestService(t, "service1")
+	speedleStore.CreatePolicy("service1", &mockPolicy)
 
 	policies, err := speedleStore.ListAllPolicies("service1", "")
-	if len(policies) != 1 {
-		t.Fatal("should have 1 policy")
+	if err != nil {
+		t.Fatal("failed to list all policies", err)
 	}
+
+	if len(policies) != 1 {
+		t.Fatal("listing policies should only return 1 policy")
+	}
+}
+
+func TestListAllPoliciesWithFilter(t *testing.T) {
+	// implement
+}
+
+func TestGetPolicyandRolePolicyCounts(t *testing.T) {
+	speedleStore.DeleteService("service1")
+	createTestService(t, "service1")
+	speedleStore.CreatePolicy("service1", &mockPolicy)
+	speedleStore.CreateRolePolicy("service1", &mockRolePolicy)
 
 	counts, err := speedleStore.GetPolicyAndRolePolicyCounts()
 	if err != nil {
@@ -111,54 +165,67 @@ func TestManagePolicies(t *testing.T) {
 	if counts["service1"].PolicyCount != 1 {
 		t.Fatal("incorrect number of policies")
 	}
-	if counts["service1"].RolePolicyCount != 0 {
+	if counts["service1"].RolePolicyCount != 1 {
 		t.Fatal("incorrect number of role policies")
 	}
+}
 
-	_, err = speedleStore.GetPolicy("service1", "nonexistantID")
-	t.Log(err)
+func TestErrorOnGetMissingPolicy(t *testing.T) {
+	speedleStore.DeleteService("service1")
+	createTestService(t, "service1")
+	speedleStore.CreatePolicy("service1", &mockPolicy)
 
+	_, err := speedleStore.GetPolicy("service1", "doesnotexist")
 	if err == nil {
-		t.Fatal("should have failed to get policy")
-	}
-
-	err = speedleStore.DeletePolicy("service1", "nonexistantID")
-	t.Log(err)
-	if err == nil {
-		t.Fatal("should have failed to delete policy")
-	}
-
-	err = speedleStore.DeletePolicy("service1", policyR.ID)
-	if err != nil {
-		t.Fatal("failed to delete policy", err)
+		t.Fatal("should have failed getting non-existing policy")
 	}
 
 }
 
-func TestManageRolePolicies(t *testing.T) {
-	//clean the service firstly
+func TestErrorOnDeletingMissingPolicy(t *testing.T) {
 	speedleStore.DeleteService("service1")
-	app := pms.Service{Name: "service1", Type: pms.TypeApplication}
-	err := speedleStore.CreateService(&app)
-	if err != nil {
-		t.Fatal("fail to create application:", err)
-	}
-	var rolePolicy pms.RolePolicy
-	rolePolicy.Name = "rp1"
-	rolePolicy.Effect = "grant"
-	rolePolicy.Roles = []string{"role1"}
-	rolePolicy.Principals = []string{"user:Alice"}
+	createTestService(t, "service1")
+	speedleStore.CreatePolicy("service1", &mockPolicy)
 
-	policyR, err := speedleStore.CreateRolePolicy("service1", &rolePolicy)
-	if err != nil {
-		t.Fatal("fail to create role policy:", err)
+	err := speedleStore.DeletePolicy("service1", "doesnotexist")
+	if err == nil {
+		t.Fatal("should have failed deleting non-existing policy")
 	}
-	policyR1, err := speedleStore.GetRolePolicy("service1", policyR.ID)
-	t.Log(policyR1)
+}
+
+func TestDeletePolicy(t *testing.T) {
+	speedleStore.DeleteService("service1")
+	createTestService(t, "service1")
+	createPolicyResult, _ := speedleStore.CreatePolicy("service1", &mockPolicy)
+	err := speedleStore.DeletePolicy("service1", createPolicyResult.ID)
 	if err != nil {
-		t.Fatal("fail to get role policy:", err)
+		t.Fatal("failed to delete policy", err)
+	}
+}
+
+func TestCreateGetRolePolicies(t *testing.T) {
+	speedleStore.DeleteService("service1")
+	createTestService(t, "service1")
+	createPolicyResult, err := speedleStore.CreateRolePolicy("service1", &mockRolePolicy)
+
+	if err != nil {
+		t.Fatal("failed to create the role policy", err)
 	}
 
+	getPolicyResult, err := speedleStore.GetRolePolicy("service1", createPolicyResult.ID)
+	if err != nil {
+		t.Fatal("failed to get the role policy from the store", err)
+	}
+
+	if getPolicyResult.Name != "rp1" {
+		t.Fatal("got the wrong role policy from the store")
+	}
+}
+
+func TestListRolePolicies(t *testing.T) {
+	speedleStore.DeleteService("service1")
+	createTestService(t, "service1")
+	_, err := speedleStore.CreateRolePolicy("service1", &mockRolePolicy)
 	rolePolicies, err := speedleStore.ListAllRolePolicies("service1", "")
 	if err != nil {
 		t.Fatal("fail to list role policies:", err)
@@ -166,57 +233,76 @@ func TestManageRolePolicies(t *testing.T) {
 	if len(rolePolicies) != 1 {
 		t.Fatal("should have 1 role policy")
 	}
+}
 
-	counts, err := speedleStore.GetPolicyAndRolePolicyCounts()
-	if err != nil {
-		t.Fatal("Fail to getCounts", err)
-	}
-	if counts["service1"].PolicyCount != 0 {
-		t.Fatal("incorrect policy number")
-	}
-	if counts["service1"].RolePolicyCount != 1 {
-		t.Fatal("incorrect role policy number")
-	}
+func TestListRolePoliciesWithFilter(t *testing.T) {
+	// implement
+}
 
-	_, err = speedleStore.GetRolePolicy("service1", "nonexistID")
-	t.Log(err)
+func TestErrorOnGetMissingRolePolicy(t *testing.T) {
+	speedleStore.DeleteService("service1")
+	createTestService(t, "service1")
+
+	_, err := speedleStore.GetRolePolicy("service1", "nonexistID")
 	if err == nil {
 		t.Fatal("should fail to get role policy")
 	}
 
-	err = speedleStore.DeleteRolePolicy("service1", "nonexistID")
-	t.Log(err)
+}
+
+func TestDeleteRolePolicy(t *testing.T) {
+	speedleStore.DeleteService("service1")
+	createTestService(t, "service1")
+	createdRolePolicy, _ := speedleStore.CreateRolePolicy("service1", &mockRolePolicy)
+
+	// make sure that it exists
+	rolePolicies, _ := speedleStore.ListAllRolePolicies("service1", "")
+	if len(rolePolicies) != 1 {
+		t.Fatal("should have 1 role policy")
+	}
+
+	// now delete it
+	err := speedleStore.DeleteRolePolicy("service1", createdRolePolicy.ID)
+	if err != nil {
+		t.Fatal("could not delete role policy")
+	}
+
+	rolePolicies, _ = speedleStore.ListAllRolePolicies("service1", "")
+	if len(rolePolicies) != 0 {
+		t.Fatal("should have 0 role policies")
+	}
+}
+
+func TestErrorOnDeleteMissingRolePolicy(t *testing.T) {
+	speedleStore.DeleteService("service1")
+	createTestService(t, "service1")
+	err := speedleStore.DeleteRolePolicy("service1", "nonexistID")
 	if err == nil {
 		t.Fatal("should fail to delete role policy")
 	}
 
-	err = speedleStore.DeleteRolePolicy("service1", policyR.ID)
+}
+
+func TestGetServiceCount(t *testing.T) {
+	speedleStore.DeleteServices()
+	createTestService(t, "service1")
+	createTestService(t, "service2")
+	serviceCount, err := speedleStore.GetServiceCount()
 	if err != nil {
-		t.Fatal("fail to delete role policy:", err)
+		t.Fatal("failed to get a count of services")
+	}
+
+	if serviceCount != 2 {
+		t.Fatalf("Service count doesn't match, expected: 2, actual: %d", serviceCount)
 	}
 }
 
-func TestCheckItemsCount(t *testing.T) {
-
-	// clean the services
+func TestGetPolicyCount(t *testing.T) {
 	speedleStore.DeleteServices()
+	createTestService(t, "service1")
+	createTestService(t, "service2")
 
-	// Create service1
-	app1 := pms.Service{Name: "service1", Type: pms.TypeApplication}
-	err := speedleStore.CreateService(&app1)
-	if err != nil {
-		t.Fatal("fail to create service:", err)
-	}
-	// Check service count
-	serviceCount, err := speedleStore.GetServiceCount()
-	if err != nil {
-		t.Fatal("Failed to get service count:", err)
-	}
-	if serviceCount != 1 {
-		t.Fatalf("Service count doesn't match, expected: 1, actual: %d", serviceCount)
-	}
-
-	// Create policies
+	// Create Policies in service 1
 	policies := []pms.Policy{
 		{Name: "p01", Effect: "grant", Principals: [][]string{{"user:user1"}}},
 		{Name: "p02", Effect: "grant", Principals: [][]string{{"user:user2"}}},
@@ -237,42 +323,7 @@ func TestCheckItemsCount(t *testing.T) {
 		t.Fatalf("Policy count doesn't match, expected:%d, actual:%d", len(policies), policyCount)
 	}
 
-	// Create Role Policies
-	rolePolicies := []pms.RolePolicy{
-		{Name: "p01", Effect: "grant", Principals: []string{"user:user1"}, Roles: []string{"role1"}},
-		{Name: "p02", Effect: "grant", Principals: []string{"user:user2"}, Roles: []string{"role2"}},
-	}
-	for _, rolePolicy := range rolePolicies {
-		_, err := speedleStore.CreateRolePolicy("service1", &rolePolicy)
-		if err != nil {
-			t.Fatal("Failed to get role policy count:", err)
-		}
-	}
-	// Check role Policy count
-	rolePolicyCount, err := speedleStore.GetRolePolicyCount("service1")
-	if err != nil {
-		t.Fatal("Failed to get the role policy count")
-	}
-	if rolePolicyCount != int64(len(rolePolicies)) {
-		t.Fatalf("RolePolicy count doesn't match, expected:%d, actual:%d", len(rolePolicies), rolePolicyCount)
-	}
-
-	// Create service2
-	app2 := pms.Service{Name: "service2", Type: pms.TypeApplication}
-	err = speedleStore.CreateService(&app2)
-	if err != nil {
-		t.Fatal("fail to create service:", err)
-	}
-	// Check service count
-	serviceCount, err = speedleStore.GetServiceCount()
-	if err != nil {
-		t.Fatal("Failed to get service count:", err)
-	}
-	if serviceCount != 2 {
-		t.Fatalf("Service count doesn't match, expected: 2, actual: %d", serviceCount)
-	}
-
-	// Create policies in service2
+	// create policies in service 2
 	for _, policy := range policies {
 		_, err := speedleStore.CreatePolicy("service2", &policy)
 		if err != nil {
@@ -287,6 +338,7 @@ func TestCheckItemsCount(t *testing.T) {
 	if policyCount != int64(len(policies)) {
 		t.Fatalf("Policy count doesn't match, expected:%d, actual:%d", len(policies), policyCount)
 	}
+
 	// Check policy count in both service1 and service2
 	policyCount, err = speedleStore.GetPolicyCount("")
 	if err != nil {
@@ -294,6 +346,33 @@ func TestCheckItemsCount(t *testing.T) {
 	}
 	if policyCount != int64(len(policies)*2) {
 		t.Fatalf("Policy count doesn't match, expected:%d, actual:%d", len(policies)*2, policyCount)
+	}
+}
+
+func TestGetRolePolicyCount(t *testing.T) {
+	speedleStore.DeleteServices()
+	createTestService(t, "service1")
+	createTestService(t, "service2")
+
+	rolePolicies := []pms.RolePolicy{
+		{Name: "p01", Effect: "grant", Principals: []string{"user:user1"}, Roles: []string{"role1"}},
+		{Name: "p02", Effect: "grant", Principals: []string{"user:user2"}, Roles: []string{"role2"}},
+	}
+
+	// Create role policies in service 1
+	for _, rolePolicy := range rolePolicies {
+		_, err := speedleStore.CreateRolePolicy("service1", &rolePolicy)
+		if err != nil {
+			t.Fatal("Failed to get role policy count:", err)
+		}
+	}
+	// Check role Policy count
+	rolePolicyCount, err := speedleStore.GetRolePolicyCount("service1")
+	if err != nil {
+		t.Fatal("Failed to get the role policy count")
+	}
+	if rolePolicyCount != int64(len(rolePolicies)) {
+		t.Fatalf("RolePolicy count doesn't match, expected:%d, actual:%d", len(rolePolicies), rolePolicyCount)
 	}
 
 	// Create rolePolicy in service2
@@ -319,18 +398,4 @@ func TestCheckItemsCount(t *testing.T) {
 	if rolePolicyCount != int64(len(rolePolicies)*2) {
 		t.Fatalf("RolePolicy count doesn't match, expected:%d, actual:%d", len(rolePolicies)*2, rolePolicyCount)
 	}
-	counts, err := speedleStore.GetPolicyAndRolePolicyCounts()
-	if err != nil {
-		t.Fatal("Fail to getCounts", err)
-	}
-	if (counts["service1"].PolicyCount != int64(len(policies))) ||
-		(counts["service2"].PolicyCount != int64(len(policies))) {
-		t.Fatal("incorrect policy number")
-	}
-	if (counts["service1"].RolePolicyCount != int64(len(rolePolicies))) ||
-		(counts["service2"].RolePolicyCount != int64(len(rolePolicies))) {
-		t.Fatal("incorrect role policy number")
-	}
-	t.Log(counts)
-
 }
